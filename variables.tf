464e7164b341a74c076179270f478c313f0d5cf4
clagosu <clagosu@users.noreply.github.com>
2022-06-22 18:57:19 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,17 @@
 variable "serviceprinciple_id" {
+  default = "749e57b9-b2af-4ce7-9135-678def554915"
 }
 
 variable "serviceprinciple_key" {
+  default = "aaG2-i~JhQzNDg02klTIZgZBPNPTqkzi~Z"
 }
 
 variable "tenant_id" {
+  default = "c506d2ac-7440-4a22-97c2-1c11f85f74af"
 }
 
 variable "subscription_id" {
+  default = "9909fced-8203-44b6-af68-11f833a37e60"
 }
 
 
