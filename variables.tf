de7f8c4036a80a9dacc589bd60652489a1b3f12d
clagosu <clagosu@users.noreply.github.com>
2022-06-28 22:55:24 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,22 @@
 variable "serviceprinciple_id" {
-  default = "749e57b9-b2af-4ce7-9135-678def554915"
+  default = ""
 }
 
 variable "serviceprinciple_key" {
-  default = "aaG2-i~JhQzNDg02klTIZgZBPNPTqkzi~Z"
+  default = ""
 }
 
 variable "tenant_id" {
-  default = "c506d2ac-7440-4a22-97c2-1c11f85f74af"
+  default = ""
 }
 
 variable "subscription_id" {
-  default = "9909fced-8203-44b6-af68-11f833a37e60"
+  default = ""
 }
 
 
 variable "ssh_key" {
- default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDkHtSOfulK3QXutZ8TNbyETLxWkDN74FmIz81XTwwoZcpKYlV7UW5eOxe+LfMAXigsYAfrGVuGEADPEu5D//wEtE9Q8V0u2CuH2VsRQhvChsB/yd6Qng3CewOcL5nWSK5DarebEPkbwB77EMmtWowBGdOok0ShNMDhiYE1JPgJbnvFFKZGBVNXRzqQBVBLfeW6w/20Oko2GYJ54jrd3jovfu57uqwgzVHYmbG4fT4AdmAdSavmxAUxryXa1A2ZOXDrBAychtWqa7ee4cBWkJaoQG3ZPjHO+f4rpCZwVDxo5+CloY5Aqm8qv3v32fDppLXuv9g4QGyNncS8ZFCFrMEupTo0duJmu/YQtDOY9aFsoN0HetNUNbfI1F088HBPSr9EwcttRpr6g6D3MYKCoRU9wpfaXW0QV8ODfL1SJKwTO0NXWDT7Mt+6YQdzjT8H5xJpzlIs8wtsLnlZrWyfEm1FOef9g9BOngwgq0ou5m8OUabIRpMV7ZgDMlJrLLbeSlVMSjpoVLj0Ql0ls8jTlHd4aKxroXQIb3B4bR78d8cTanPi6QWjMM5xCntFQ2kRN/z+8ofzLefeDUGr0JmEKniXhe/ixqIvdlfuSiNgz2ynpjAKXjmaSLgY9wdehQHTvMs9a+83GFI8741PKgSoYohwE/M9Oz7LxUOsbr8I65sDcw== clagos353@gmail.com"
+ default = ""
 }
 
 variable "location" {
